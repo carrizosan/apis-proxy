a45a9f9e6c94ceaef83cf6050bbdc5e71cd9d3fc
Santiago Carrizo <santiicaarrizo@gmail.com>
2023-07-17 07:51:01 -0300

    feat: change rate limiters store to redis

--- src/middlewares/rate-limiters.middleware.js
+++ src/middlewares/rate-limiters.middleware.js
@@ -1,4 +1,6 @@
 const rateLimit = require('express-rate-limit');
+const RedisStore = require('rate-limit-redis');
+const { redisClient } = require('../db/init-redis');
 
 const DEFAULT_WINDOW_TIME = 60 * 60 * 1000; // 1 hour window default
 const DEFAULT_MAX_REQUESTS = 1000; // 1000 requests by window default
@@ -8,6 +10,9 @@ const ipRateLimiter = rateLimit({
   max: process.env.IP_RATE_LIMIT_MAX_REQUESTS || DEFAULT_MAX_REQUESTS, // Number of requests by window
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
 });
 
 const pathRateLimiter = rateLimit({
@@ -16,6 +21,9 @@ const pathRateLimiter = rateLimit({
   standardHeaders: true,
   legacyHeaders: false,
   keyGenerator: (req) => req.path,
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
 });
 
 const ipAndPathRateLimiter = rateLimit({
@@ -24,6 +32,9 @@ const ipAndPathRateLimiter = rateLimit({
   standardHeaders: true,
   legacyHeaders: false,
   keyGenerator: (req) => `${req.ip}:${req.path}`,
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
 });
 
 module.exports = { ipRateLimiter, pathRateLimiter, ipAndPathRateLimiter };
